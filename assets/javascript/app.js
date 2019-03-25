042caa0cb59a74f52ff5589d8361c89b1da55938
Andrew Bayles <baylesweb@gmail.com>
2019-03-25 02:03:48 -0500

    Refactoring Update
    Basic refactoring, for additional DRY-ness.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,65 +1,10 @@
-// Use compound arrays to organize trivia data?
-// Possible answers are displayed randomly, but in their array, index 0 is always correct.
-
 let playerRight = 0;
 let playerWrong = 0;
 let currentQuestion = 0;
 let clockRunning = false;
 let questionClockTime;
 
-let questionData = [ // The correct answer for every question is always the first in questionData[1].
-
-	[ 'Which of the following game series was developed by Japanese company SquareSoft?', 
-		[ 'Final Fantasy', 'Super Mario Bros.', 'The Legend of Zelda', 'Metroid' ],
-		[ 'final_fantasy.jpg' ]
-	],
-
-	[ 'Which of the following characters was featured in a game produced for the original 8-bit Nintendo Entertainment System?', 
-		[ 'Simon Belmont', 'Sonic the Hedgehog', 'Tony Stark', 'Captain EO' ],
-		[ 'simon_belmont.png' ] 
-	],
-
-	[ 'The title of the first First Person Shooter published for home consoles was:',
-		[ 'GoldenEye', 'Quake', 'Wolfenstein', 'Half-Life' ],
-		[ 'goldeneye.jpg' ]
-	],
-
-	[ 'The title of the first game which served as a demo for the Super Nintendo\'s then-revolutionary \"Mode 7\" graphics technology was:',
-		[ 'Pilotwings', 'Donkey Kong Country', 'Lufia: The Fortress of Doom', 'Super Mario World' ],
-		[ 'pilotwings.jpg' ]
-	],
-
-	[ 'A game which was the subject of repeat controversy in the nineties for its intense gore and violence was titled:',
-		[ 'Mortal Kombat', 'Street Fighter II', 'Pit Fighter', 'Super Smash Brothers' ],
-		[ 'mortal_kombat.jpg' ]
-	],
-
-	[ 'The first shooter game to feature fully three-dimensonally rendered opponents was called:',
-		[ 'Quake', 'Myst', 'Space Quest IV', 'Doom' ],
-		[ 'quake_cover.jpg' ]
-	],
-
-	[ 'The protagonist\'s name in the 21st-century PC game series \"The Witcher\" is:',
-		[ 'Geralt of Rivia', 'Roger Wilco', 'Terra Branford', 'Edward Snowden' ],
-		[ 'geralt_of_rivia.png' ]
-	],
-
-	[ 'The 16-bit home console produced by NEC Home Electronics and released in North America in 1989 was called:',
-		[ 'Turbografx', 'Genesis', 'Wii', 'Virtual Boy' ],
-		[ 'turbografx-16.jpg' ]
-	],
-
-	[ 'Nintendo\'s proprietary gaming magazine was originally titled:',
-		[ 'Nintendo Power', 'GamePro Magazine', 'Game Informer', 'Kotaku' ],
-		[ 'nintendo_power.jpg' ]
-	],
-
-	[ 'The first 64-bit home console, the N64, was released in which year?',
-		[ '1996', '1994', '1992', '1997' ],
-		[ 'nintendo-64.jpg' ]
-	]
-
-];
+// Trivia data is organized externally. See assets/javascript/trivia_data.js
 
 let randomData = shuffleArray( questionData ); // Randomize questions.
 
@@ -70,9 +15,7 @@ function startScreen() { // Start screen. Initialize the game.
 	playerRight = 0;
 	currentQuestion = 0;
 
-	$("#question-screen").css( "display", "none" );
-	$("#answer-screen").css( "display", "none" );
-	$("#final-page").css( "display", "none" );
+	clearScreens();
 
 	$("#start-screen").css( "display", "block" );
 
@@ -85,17 +28,13 @@ function startScreen() { // Start screen. Initialize the game.
 
 function displayQuestion( question ) { // Question screen. Takes the next question from a randomized array and displays it, with four randomized answers.
 
-	$("#start-screen").css( "display", "none" );
-	$("#answer-screen").css( "display", "none" );
+	clearScreens();
 
-	$("#question-screen h4").text( question[0] );
-
+	$("#question-screen h4").text( question[0] ); // Display current question from randomized array.
 	$("#question-screen ul li").remove(); // Remove all previous displayed options.
-
 	$("#display-clock").text("00:15");
 
 	randomAnswer = shuffleArray( [0, 1, 2, 3] );
-	
 	for ( i = 0; i < 4; i++ ) {
 		$("#question-screen ul").append( '<li value="' + randomAnswer[i] + '">' + question[1][randomAnswer[i]] + '</li>' );
 	}
@@ -132,13 +71,12 @@ function displayQuestion( question ) { // Question screen. Takes the next questi
 
 function displayAnswer( question, result ) { // Displays the correct answer for a limited time, with associated image.
 
+	clearScreens();
+
 	clearInterval( clockInterval ); // Reset question countdown timer.
 	questionClockTime = 0;
 	 $("#display-clock").text("00:00");
 	clockRunning = false;
-
-	$("#start-screen").css( "display", "none" );
-	$("#question-screen").css( "display", "none" );
 
 	if ( result === 0 ) {
 		$("#answer-screen h4").text( "Oops! You ran out of time." );
@@ -171,8 +109,7 @@ function displayAnswer( question, result ) { // Displays the correct answer for 
 
 function finalPage() {
 
-	$("#question-screen").css( "display", "none" );
-	$("#answer-screen").css( "display", "none" );
+	clearScreens();
 
 	$("#total-correct span").text( playerRight );
 	$("#total-incorrect span").text( playerWrong );
@@ -184,6 +121,15 @@ function finalPage() {
 		startScreen();
 	});
 
+}
+
+
+// Clear all displayed screens in preparation for the next one.
+function clearScreens() {
+	$("#start-screen").css( "display", "none" );
+	$("#question-screen").css( "display", "none" );
+	$("#answer-screen").css( "display", "none" );
+	$("#final-page").css( "display", "none" );
 }
 
 
---
+++ assets/javascript/trivia_data.js
@@ -0,0 +1,53 @@
+let questionData = [ // The correct answer for every question is always the first in questionData[1].
+
+	[ 'Which of the following game series was developed by Japanese company SquareSoft?', 
+		[ 'Final Fantasy', 'Super Mario Bros.', 'The Legend of Zelda', 'Metroid' ],
+		[ 'final_fantasy.jpg' ]
+	],
+
+	[ 'Which of the following characters was featured in a game produced for the original 8-bit Nintendo Entertainment System?', 
+		[ 'Simon Belmont', 'Sonic the Hedgehog', 'Tony Stark', 'Captain EO' ],
+		[ 'simon_belmont.png' ] 
+	],
+
+	[ 'The title of the first First Person Shooter published for home consoles was:',
+		[ 'GoldenEye', 'Quake', 'Wolfenstein', 'Half-Life' ],
+		[ 'goldeneye.jpg' ]
+	],
+
+	[ 'The title of the first game which served as a demo for the Super Nintendo\'s then-revolutionary \"Mode 7\" graphics technology was:',
+		[ 'Pilotwings', 'Donkey Kong Country', 'Lufia: The Fortress of Doom', 'Super Mario World' ],
+		[ 'pilotwings.jpg' ]
+	],
+
+	[ 'A game which was the subject of repeat controversy in the nineties for its intense gore and violence was titled:',
+		[ 'Mortal Kombat', 'Street Fighter II', 'Pit Fighter', 'Super Smash Brothers' ],
+		[ 'mortal_kombat.jpg' ]
+	],
+
+	[ 'The first shooter game to feature fully three-dimensonally rendered opponents was called:',
+		[ 'Quake', 'Myst', 'Space Quest IV', 'Doom' ],
+		[ 'quake_cover.jpg' ]
+	],
+
+	[ 'The protagonist\'s name in the 21st-century PC game series \"The Witcher\" is:',
+		[ 'Geralt of Rivia', 'Roger Wilco', 'Terra Branford', 'Edward Snowden' ],
+		[ 'geralt_of_rivia.png' ]
+	],
+
+	[ 'The 16-bit home console produced by NEC Home Electronics and released in North America in 1989 was called:',
+		[ 'Turbografx', 'Genesis', 'Wii', 'Virtual Boy' ],
+		[ 'turbografx-16.jpg' ]
+	],
+
+	[ 'Nintendo\'s proprietary gaming magazine was originally titled:',
+		[ 'Nintendo Power', 'GamePro Magazine', 'Game Informer', 'Kotaku' ],
+		[ 'nintendo_power.jpg' ]
+	],
+
+	[ 'The first 64-bit home console, the N64, was released in which year?',
+		[ '1996', '1994', '1992', '1997' ],
+		[ 'nintendo-64.jpg' ]
+	]
+
+];
